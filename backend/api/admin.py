e0f88f4f45eb09cd39d467fcab131c7475808858
eidoru <adrian.gerbise@proton.me>
2024-05-16 23:01:18 +0800

    Added custom forms and UserAdmin

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,8 +1,21 @@
 from django.contrib import admin
-from . import models
+
+from . import models, forms
 
 
-admin.site.register(models.User)
+class UserAdmin(admin.ModelAdmin):
+    form = forms.UserChangeForm
+    add_form = forms.UserCreationForm
+
+    def get_form(self, request, obj=None, **kwargs):
+        if obj is None:
+            return self.add_form
+        return super().get_form(request, obj, **kwargs)
+
+    list_display = ["username", "first_name", "last_name", "role"]
+
+
+admin.site.register(models.User, UserAdmin)
 admin.site.register(models.Restaurant)
 admin.site.register(models.MenuItem)
 admin.site.register(models.Cart)
