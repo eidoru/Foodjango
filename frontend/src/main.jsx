1f5f8f7f559230ddad36e7189e92994aecccef1c
eidoru <adrian.gerbise@proton.me>
2024-05-27 00:47:50 +0800

    Wrapped router to auth provider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,13 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
 import { RouterProvider } from "react-router-dom";
 import browserRouter from "./routes";
 
+import AuthProvider from "./providers/AuthProvider";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <AuthProvider>
     <RouterProvider router={browserRouter} />
-  </React.StrictMode>,
+  </AuthProvider>,
 );
